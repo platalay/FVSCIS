f81bb7488d43898af0a6fed7c08c2375b1a1e3bf
platalay <platalay@gmail.com>
2025-06-30 17:09:08 +0700

    start signer

---
+++ FVSCIS/signer/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../../private/initialize.php');
+$session->require_role(['signer']);
+include("../../private/shared/headerofficer.php");
+include("../../private/shared/sidebarofficer.php");
+include("../../private/shared/topbarofficer.php");
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+  <h1 class="h3 mb-4 text-gray-800">หน้าผู้ลงนามใบรับรอง (signer)</h1>
+</div>
+
+<?php
+include("../../private/shared/footerofficer.php");
+include("../../private/shared/footerall.php");
+?>
